85439e3f4c7a93176ffebc85ed07a84c6dda13da
huxiaoxu2019 <admin@ihuxu.com>
2020-10-26 23:08:45 +0800

    :wrench: add codes

--- basic-algorithm/sort/quick-sort.cpp
+++ basic-algorithm/sort/quick-sort.cpp
@@ -1,6 +1,4 @@
-/**
- * https://www.acwing.com/activity/content/code/content/39784/
- */
+// https://www.luogu.com.cn/problem/P1177
 #include <iostream>
 
 using namespace std;
@@ -8,24 +6,24 @@ using namespace std;
 const int N = 100010;
 int a[N];
 
-void quick_sort(int a[], int s, int e) {
-    if (s >= e) return;
-    int l = s - 1, r = e + 1, m = a[s + e >> 1];
-    while (l < r) {
-        while(a[++l] < m);
-        while(a[--r] > m);
-        if (l < r) swap(a[l], a[r]);
+void quick_sort(int l, int r) {
+    if (l >= e) return;
+    int i = l - 1, j = r + 1, m = a[l + r >> 1];
+    while (i < j) {
+        while (a[++i] < m);
+        while (a[--j] > m);
+        if (i < j) swap(a[i], a[j]);
     }
-    quick_sort(a, s, r);
-    quick_sort(a, r + 1, e);
+    quick_sort(l, j);
+    quick_sort(j + 1, r);
+    // j,j+1,l
 }
 
 int main() {
     int n;
     cin >> n;
-
     for (int i = 0; i < n; ++i) cin >> a[i];
-    quick_sort(a, 0, n - 1);
+    quick_sort(0, n - 1);
     for (int i = 0; i < n; ++i) cout << a[i] << " ";
     return 0;
 }
