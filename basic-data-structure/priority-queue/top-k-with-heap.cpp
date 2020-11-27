83949fe3a2a16fcc659187a8b142cb3ce353cb28
huxiaoxu2019 <admin@ihuxu.com>
2020-11-27 13:39:26 +0800

    :wrench: codes

--- basic-data-structure/priority-queue/top-k-with-heap.cpp
+++ basic-data-structure/priority-queue/top-k-with-heap.cpp
@@ -1,44 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e5 + 5;
-int Size, h[N];
+int h[N], Size;
+// 小根堆
 void up(int u) {
-    while (u/2 > 0 && h[u/2] > h[u]) {
+    while(u/2 > 0 && h[u/2] > h[u]) {
         swap(h[u/2], h[u]);
-        u/=2;
+        u = u>>1;
     }
 }
 void down(int u) {
-    while (u<Size) {
+    while (u < Size) {
         int v = u;
-        if (u*2 <= Size && h[v] > h[u*2]) v=u*2;
-        if (u*2+1 <= Size && h[v] > h[u*2+1]) v=u*2+1;
-        if (u!=v) {
-            swap(h[u], h[v]);
-            u=v;
-        } else {
-            break;
-        }
+        if (u * 2 <= Size && h[v] > h[u * 2]) v = u * 2;
+        if (u * 2 + 1 <= Size && h[v] > h[u * 2 + 1]) v = u * 2 + 1;
+        if (u == v) break;
+        swap(h[u], h[v]);
+        u = v;
     }
 }
+void push(int u) {
+    h[++Size] = u;
+    up(Size);
+}
 int top() {
-    return Size > 0 ? h[1] : 0;
+    return Size >= 1 ? h[1] : 0;
 }
 void pop() {
     h[1] = h[Size--];
     down(1);
 }
-void push(int x) {
-    h[++Size]=x;
-    up(Size);
-}
 int main() {
     vector<int> a{3,1,2,4,5};
     int n = a.size();
     int k = 2; // 第 K 大
+    // codes...
     for (int i = 0; i < n; ++i) {
-        if (Size < k || top() < a[i]) {
-            push(a[i]);
+        int t = a[i];
+        if (Size < k || top() < t) {
+            push(t);
         }
         if (Size > k) pop();
     }
