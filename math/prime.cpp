bda1d067a11b1f81e97a974d4f964a0c73f8d34d
huxiaoxu2019 <admin@ihuxu.com>
2021-08-31 18:25:55 +0800

    :wrench: add codes

--- math/prime.cpp
+++ math/prime.cpp
@@ -1,26 +1,38 @@
 // https://blog.csdn.net/zzzanj/article/details/81260098
 #include<bits/stdc++.h>
 using namespace std;
-// O(n) 欧拉筛法 线性
-const long long maxn=1e8+1;
+const long long maxn=1e5+1;
 long long prime[maxn];
-bool sf[maxn]; // suffix for prime
+bool st[maxn];
+long long cnt=0;
+
+// O(n) 欧拉筛法
 void table() {
-    long long num=0;
-    memset(sf, true, sizeof sf);
-    sf[1] = false;
-    sf[0] = false;
     for (int i=2;i<=maxn;++i) {
-        if (sf[i]) prime[++num]=i;
-        for (int j=1;j<=num;++j) {
+        if (!st[i]) prime[++cnt]=i;
+        for (int j=1;j<=cnt;++j) {
             if (i*prime[j]>maxn) break;
-            sf[i*prime[j]]=false;
+            st[i*prime[j]]=true;
             if (i%prime[j] == 0) break;
         }
     }
 }
 
-int main() {
-    return 0;
+
+// 埃及筛法 O(nloglogn)
+void table_2() {
+    for (int i = 2; i <= maxn; ++i) {
+        if (!st[i]) {
+            prime[++cnt] = i;
+            for (int j = i; j <= maxn; j += i) {
+                st[j] = true;
+            }
+        }
+    }
 }
 
+int main() {
+    table_2();
+    for (int i=1;i<=10;++i) cout<<prime[i]<<endl;
+    return 0;
+}
