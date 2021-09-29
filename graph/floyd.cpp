49016910d74d7e02be2ac8f8160b976f9b30f007
huxiaoxu2019 <admin@ihuxu.com>
2021-09-29 23:38:31 +0800

    :wrench: add codes

---
+++ graph/floyd.cpp
@@ -0,0 +1,36 @@
+// https://www.cnblogs.com/Ash-ly/p/5920953.html
+#include<bits/stdc++.h>
+using namespace std;
+const int N=210,M=1e5+10,INF=1e9;
+int g[N][N],n,m,q,k;
+void floyd() {
+    for (int k=1;k<=n;++k) {
+        for (int i=1;i<=n;++i) {
+            for (int j=1;j<=n;++j) {
+                g[i][j] = min(g[i][j], g[i][k]+g[k][j]);
+            }
+        }
+    }
+}
+int main() {
+    cin>>n>>m>>k;
+    for (int i=1;i<=n;++i) {
+        for (int j=1;j<=n;++j) {
+            if (i==j) g[i][j]=0;
+            else g[i][j]=INF;
+        }
+    }
+    for (int i=0;i<m;++i) {
+        int a,b,c;
+        cin>>a>>b>>c;
+        g[a][b]=min(g[a][b],c);
+    }
+    floyd();
+    for (int i=0;i<k;++i) {
+        int a,b;
+        cin>>a>>b;
+        if (g[a][b] > INF / 2) cout<<"impossible"<<endl;
+        else cout<<g[a][b]<<endl;
+    }
+    return 0;
+}
