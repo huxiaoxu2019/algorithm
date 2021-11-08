8e1b5a3bd59839acaf0de47abd0ee78dfcbaa118
huxiaoxu2019 <admin@ihuxu.com>
2021-11-08 13:38:00 +0800

    :wrench: update code

--- graph/dijkstra-priority-queue.cpp
+++ graph/dijkstra-priority-queue.cpp
@@ -1,6 +1,8 @@
+// TC: O(mn), m for edges, n for vers
+// TIP: The point with the smallest distance from the starting point is updated each time
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e5+10+5e4;
+const int N=1e5+10+5e4; // be careful if the const N is enough
 int head[N],Next[N],tot,ver[N],edg[N];
 int n,m,d[N],vis[N];
 // first is distance, second is no. big-root heap for default.
@@ -14,15 +16,15 @@ void add(int a, int b, int c) {
 
 void dijkstra() {
     memset(d, 0x3f, sizeof d);
-    d[1]=0;
+    d[1]=0; // remember to init the start point dist to 0
     pq.push({0,1});
     while (!pq.empty()) {
         auto pa=pq.top(); pq.pop();
         auto u=pa.second;
-        if (vis[u]) continue;
+        if (vis[u]) continue; // just use once
         // vis for de-duplication, variable name is best to use vis not v.
         vis[u]=1;
-        for (int i=head[u];i!=-1;i=Next[i]) {
+        for (int i=head[u];i;i=Next[i]) {
             auto v=ver[i], w=edg[i];
             if (d[v] > d[u]+w) {
                 d[v]=d[u]+w;
@@ -39,7 +41,8 @@ void dijkstra() {
 
 int main() {
     // remember for where to set the head var!!!
-    memset(head, -1, sizeof head);
+    // if not set to -1 value, the tot must increase from 1 index
+    // memset(head, -1, sizeof head);
     cin>>n>>m;
     for (int i=0;i<m;++i) {
         int a,b,c;
