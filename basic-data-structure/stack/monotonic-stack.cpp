e0edbf1856275cc6928f79ad5942531d2320c939
huxiaoxu2019 <admin@ihuxu.com>
2021-11-12 13:20:22 +0800

    :feature: add codes

--- basic-data-structure/stack/monotonic-stack.cpp
+++ basic-data-structure/stack/monotonic-stack.cpp
@@ -1,17 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 10;
-int a[N], n, s[N], t; // s[1 2 3 ...]
+const int N=1e5+10;
+int q[N], tt=0; // from index 1, q[tt] = value
+int n, t;
 int main() {
-    cin >> n;
-    s[0] = -1;
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        while (t && a[s[t]] >= a[i]) --t;
-        s[++t] = i;
-        cout << s[t - 1] << " ";
+    cin>>n;
+    while (n--) {
+        cin>>t;
+        while (tt && q[tt] >= t) --tt;
+        if (tt) {
+            cout<<q[tt]<<" ";
+        } else {
+            cout<<"-1 ";
+        }
+        q[++tt] = t;
     }
     return 0;
 }
