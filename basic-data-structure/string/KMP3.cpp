6f3784ecdb17a4bbcc035e176e7a4030a42f7842
huxiaoxu2019 <admin@ihuxu.com>
2021-11-15 21:06:39 +0800

    :wrench: codes

---
+++ basic-data-structure/string/KMP3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+int n,m, Next[N];
+string s,p; // n = s.size(), m = p.size()
+vector<int> ans;
+void getNext() {
+    for (int i=0,j=1;j<n;++j) {
+        while (i > 0 && p[i] != p[j]) i=Next[i-1];
+        if (p[i] == p[j]) ++i;
+        Next[j] = i;
+    }
+}
+void kmp() {
+    for (int i=0,j=0;i<m;++i) {
+        while (j > 0 && s[i] != p[j]) j = Next[j-1];
+        if (s[i] == p[j]) ++j;
+        if (j == n) {
+            //cout<<"i:"<<i<<" j:"<<j<<endl;
+            ans.push_back(i - n + 1);
+            j = Next[j - 1];
+        }
+    }
+}
+
+
+int main() {
+    cin>>n>>p>>m>>s;
+    getNext();
+    kmp();
+    for (auto c : ans) cout << c << " ";
+    return 0;
+}
