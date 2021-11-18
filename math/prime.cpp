62fe8ac1230de07b8a8350526a5a9ceb03a6c31a
huxiaoxu2019 <admin@ihuxu.com>
2021-11-18 13:44:14 +0800

    :feature: add codes

--- math/prime.cpp
+++ math/prime.cpp
@@ -1,38 +1,33 @@
-// https://blog.csdn.net/zzzanj/article/details/81260098
 #include<bits/stdc++.h>
 using namespace std;
-const long long maxn=1e5+1;
-long long prime[maxn];
-bool st[maxn];
-long long cnt=0;
-
-// O(n) 欧拉筛法
-void table() {
-    for (int i=2;i<=maxn;++i) {
-        if (!st[i]) prime[++cnt]=i;
-        for (int j=1;j<=cnt;++j) {
-            if (i*prime[j]>maxn) break;
-            st[i*prime[j]]=true;
-            if (i%prime[j] == 0) break;
-        }
-    }
-}
-
-
-// 埃及筛法 O(nloglogn)
-void table_2() {
-    for (int i = 2; i <= maxn; ++i) {
+const int N=1e6+10;
+int st[N],prime[N],cnt;
+int n;
+void getPrimes(int n) {
+    // 埃及筛法
+    for (int i=2;i<=n;++i) {
         if (!st[i]) {
-            prime[++cnt] = i;
-            for (int j = i; j <= maxn; j += i) {
-                st[j] = true;
+            prime[++cnt]=i;
+            for (int j=prime[cnt];j<=n;j+=prime[cnt]) {
+                st[j]=true;
             }
         }
     }
+    cout<<cnt<<endl;
 }
-
+// 线性筛法 - 每一个质数用最小的质因子筛掉
+void getPrimes2(int n) {
+    for (int i=2;i<=n;++i) {
+        if (!st[i]) prime[++cnt]=i;
+        for (int j=1;prime[j]*i<=n;++j) {
+            st[prime[j]*i]=true;
+            if (i%prime[j]==0) break;
+        }
+    }
+    cout<<cnt<<endl;
+}
 int main() {
-    table_2();
-    for (int i=1;i<=10;++i) cout<<prime[i]<<endl;
+    cin>>n;
+    getPrimes2(n);
     return 0;
 }
