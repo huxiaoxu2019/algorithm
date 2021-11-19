bc231ff86c7dac1962055f78e043142f461a1d34
huxiaoxu2019 <admin@ihuxu.com>
2021-11-19 19:02:26 +0800

    :wrench: add codes

--- math/fast-power.cpp
+++ math/fast-power.cpp
@@ -1,4 +1,6 @@
 // 费马小定理 https://zh.wikipedia.org/wiki/%E8%B4%B9%E9%A9%AC%E5%B0%8F%E5%AE%9A%E7%90%86
+// b 与 p 互质，则有 b^(p-1)与1同余，在模p的情况下
+// b*b^(p-2)与1同余，则b^(p-2)为逆元
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
