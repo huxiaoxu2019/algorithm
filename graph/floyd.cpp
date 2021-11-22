1a59a1b7561a5d9ff8083ea09c019483a69c6550
huxiaoxu2019 <admin@ihuxu.com>
2021-11-22 23:16:50 +0800

    :wrench: add code

--- graph/floyd.cpp
+++ graph/floyd.cpp
@@ -1,9 +1,12 @@
+// TC: O(n^3)
 // https://www.cnblogs.com/Ash-ly/p/5920953.html
 #include<bits/stdc++.h>
 using namespace std;
 const int N=210,M=1e5+10,INF=1e9;
 int g[N][N],n,m,q,k;
 void floyd() {
+    // g[k][i][j] = min{g[k-1][i][k], g[k-1][k][j]}
+    // 经过需要 1-k 个点的情况下，从 i 到 j 的最短距离
     for (int k=1;k<=n;++k) {
         for (int i=1;i<=n;++i) {
             for (int j=1;j<=n;++j) {
--- graph/spfa.cpp
+++ graph/spfa.cpp
@@ -14,19 +14,19 @@ void spfa() {
     q[++tt]=1;
     vis[1]=1;
     d[1]=0;
-    while (hh<=tt) {
+    while (hh<=tt) { // hh <= tt
         auto u=q[hh++];
         vis[u]=0; // remember set flag to false while removed from queue
         for (int i=head[u];i!=-1;i=Next[i]) {
             auto v=ver[i], w=edge[i];
-            if (d[v]>d[u]+w) {
+            if (d[v]>d[u]+w) { // d[v] > d[u] + w
                 d[v]=d[u]+w;
                 if (!vis[v]) q[++tt]=v;
                 vis[v]=1;
             }
         }
     }
-    if (d[n] > 0x3f3f3f3f/2) {
+    if (d[n] != 0x3f3f3f3f) { // != 0x3f3f3f3f
         cout<<"impossible"<<endl;
     } else {
         cout<<d[n]<<endl;
